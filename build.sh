07249e77cb3412ba9859be1de14d13b652ea5c8d
jercoh <jejechaber289@msn.com>
2017-03-22 08:39:26 -0400

    fix if resources is deleted

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ mkdir resources
 wget "http://bailando.sims.berkeley.edu/enron/enron_with_categories.tar.gz" -O ./resources/enron_with_categories.tar.gz
 gunzip ./resources/enron_with_categories.tar.gz
 java -jar tar-to-seq/tar-to-seq.jar ./resources/enron_with_categories.tar ./resources/enron_mails.seq
+docker-compose down
 docker-compose build
 docker-compose up -d
 docker-compose scale spark-worker=2
